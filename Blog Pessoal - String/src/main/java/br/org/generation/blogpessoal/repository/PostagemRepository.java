0594937062afa9255bfcfa33dc635712629a0721
unknown <gabriel,maia1333@gmail.com>
2022-01-20 21:55:27 -0300

    blog pessoal.

--- Blog Pessoal - String/src/main/java/br/org/generation/blogpessoal/repository/PostagemRepository.java
+++ Blog Pessoal - String/src/main/java/br/org/generation/blogpessoal/repository/PostagemRepository.java
@@ -4,16 +4,15 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import br.org.generation.blogpessoal.model.Postagem;
 
 
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
-	
-	
-	List<Postagem> findAllByTituloContainingIgnoreCase(String Titulo);
-	
-	/* select * from tb_postagens where titulo like %titulo% */
 
+
+	public List <Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+	
 
 }
